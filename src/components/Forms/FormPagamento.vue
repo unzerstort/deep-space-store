253b026c882af4e1ed2792dc2cd0a64896b50472
Sandra Bastos <sandra@vermillion.net.br>
2024-02-17 11:35:40 -0300

    adds value prop to radio buttons

--- src/components/Forms/FormPagamento.vue
+++ src/components/Forms/FormPagamento.vue
@@ -19,9 +19,9 @@ export default {
     <VContainer>
         <VForm>
             <VRadioGroup label="Selecione o meio de pagamento:">
-                <VRadio label="Boleto"></VRadio>
-                <VRadio label="Cartão de crédito"></VRadio>
-                <VRadio label="Pix"></VRadio>
+                <VRadio label="Boleto" value="bill"></VRadio>
+                <VRadio label="Cartão de crédito" value="card"></VRadio>
+                <VRadio label="Pix" value="pix"></VRadio>
             </VRadioGroup>
 
             <VTextField label="Informe o CPF" type="number"></VTextField>
