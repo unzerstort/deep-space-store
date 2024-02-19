5086670312d5a5aceac8a3d3531fa3ce42cff472
Sandra Bastos <sandra@vermillion.net.br>
2024-02-18 23:30:05 -0300

    fixes alignment between components and deletes unused file

--- src/components/Content/OfferCard.vue
+++ src/components/Content/OfferCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { VStepper } from 'vuetify/components'
+import { VRow, VCol, VStepper } from 'vuetify/components';
 import MainCard from './MainCard.vue';
 import FormPersonal from "@/components/Forms/FormPersonal.vue";
 import FormDelivery from "@/components/Forms/FormDelivery.vue";
@@ -18,29 +18,36 @@ export default {
 </script>
 
 <template>
-    <OfferDetail :offer="offer"></OfferDetail>
-    <VStepper 
-        alt-labels 
-        editable
-        :items="['Dados pessoais', 'Dados de entrega', 'Forma de pagamento']"
-    >
+    <VRow>
+        <VCol cols="3">
+            <OfferDetail :offer="offer"></OfferDetail>
+        </VCol>
 
-        <template v-slot:item.1>
-            <MainCard>
-                <FormPersonal></FormPersonal>
-            </MainCard>
-        </template>
-
-        <template v-slot:item.2>
-            <MainCard>
-                <FormDelivery></FormDelivery>
-            </MainCard>
-        </template>
-
-        <template v-slot:item.3>
-            <MainCard>
-                <FormPayment :paymentOptions="offer.paymentOptions"></FormPayment>
-            </MainCard>
-        </template>
-    </VStepper>
+        <VCol cols="9">
+            <VStepper 
+                alt-labels 
+                editable
+                :items="['Dados pessoais', 'Dados de entrega', 'Forma de pagamento']"
+            >
+        
+                <template v-slot:item.1>
+                    <MainCard>
+                        <FormPersonal></FormPersonal>
+                    </MainCard>
+                </template>
+        
+                <template v-slot:item.2>
+                    <MainCard>
+                        <FormDelivery></FormDelivery>
+                    </MainCard>
+                </template>
+        
+                <template v-slot:item.3>
+                    <MainCard>
+                        <FormPayment :paymentOptions="offer.paymentOptions"></FormPayment>
+                    </MainCard>
+                </template>
+            </VStepper>
+        </VCol>
+    </VRow>
 </template>
--- src/components/Content/OfferDetail.vue
+++ src/components/Content/OfferDetail.vue
@@ -16,54 +16,44 @@ export default {
     <VCard>
         <VCardTitle>{{ offer.name }}</VCardTitle>
         <VCardSubtitle>{{ offer.author }}</VCardSubtitle>
-        <VRow>
-            <VCol cols="2" align-self="center">
-                <VImg 
-                    :alt="offer.altText"
-                    :src="offer.image" 
-                    :lazy-src="offer.image" 
-                    aspect-ratio="1/1" 
-                    height="200" width="200"
-                >
-                    <template v-slot:error>
-                        <VImg
-                            height="200"
-                            max-width="200"
-                                                src="../../assets/imgs/no-img-available.jpg"
-                        ></VImg>
-                    </template>
+        <VImg 
+            :alt="offer.altText"
+            :src="offer.image" 
+            :lazy-src="offer.image" 
+            height="200" width="200"
+        >
+            <template v-slot:error>
+                <VImg
+                    height="200"
+                    max-width="200"
+                    src="../../assets/imgs/no-img-available.jpg"
+                ></VImg>
+            </template>
 
-                </VImg>
+        </VImg>
 
-            </VCol>
-                
-            <VCol cols="2">
-                <VCardText>
-        
-                    <p>
-                        {{offer.description}}
-                    </p>
-                    
-                    <p>
-                        <strong>Dimensões: </strong>{{offer.dimensions}}
-                    </p>
-        
-                    <p>
-                        <strong>Páginas: </strong>{{ offer.pages }}
-                    </p>
-        
-                    <p>
-                        <strong>Idioma: </strong>{{ offer.language }}
-                    </p>
-                    
-                    <p>
-                        <strong>Editora: </strong> {{ offer.publisher }}
-                    </p>
-                </VCardText>
+        <VCardText>
 
-            </VCol>
-        </VRow>
-        <VCardActions></VCardActions>
+            <p>
+                {{offer.description}}
+            </p>
+            
+            <p>
+                <strong>Dimensões: </strong>{{offer.dimensions}}
+            </p>
+
+            <p>
+                <strong>Páginas: </strong>{{ offer.pages }}
+            </p>
+
+            <p>
+                <strong>Idioma: </strong>{{ offer.language }}
+            </p>
+            
+            <p>
+                <strong>Editora: </strong> {{ offer.publisher }}
+            </p>
+        </VCardText>
     </VCard> 
 
 </template>
