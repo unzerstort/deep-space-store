fd6c3dd89791cf958a6ac36b6812d05d0e40bb5a
Sandra Bastos <sandra@vermillion.net.br>
2024-02-19 16:59:43 -0300

    centers bar code and qr-code

--- src/components/Payment/BillPayment.vue
+++ src/components/Payment/BillPayment.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { VImg } from 'vuetify/components';
+import { VRow, VImg } from 'vuetify/components';
 
 </script>
 
 <template>
-    <VImg 
-        src="../src/assets/imgs/bar-code.png"
-        lazy-src="../src/assets/imgs/bar-code.png"
-        height="200" width="600"
-    >
-    </VImg>
+    <VRow justify="center" align="center">
+        <VImg 
+            src="../src/assets/imgs/bar-code.png"
+            lazy-src="../src/assets/imgs/bar-code.png"
+            height="200" width="600"
+        >
+        </VImg>
+    </VRow>
 </template>
--- src/components/Payment/PixPayment.vue
+++ src/components/Payment/PixPayment.vue
@@ -1,12 +1,14 @@
 <script setup>
-import { VImg } from 'vuetify/components';
+import { VRow, VImg } from 'vuetify/components';
 
 </script>
 
 <template>
-    <VImg
-        src="../src/assets/imgs/qr-code.jpg"
-        height="400" width="400"
-    >
-    </VImg>
+    <VRow justify="center" align="center">
+        <VImg
+            src="../src/assets/imgs/qr-code.jpg"
+            height="400" width="400"
+        >
+        </VImg>
+    </VRow>
 </template>
