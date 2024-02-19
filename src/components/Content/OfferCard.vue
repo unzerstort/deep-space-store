a5c2514363907cc0c0197c33a16df541622c5f1a
Sandra Bastos <sandra@vermillion.net.br>
2024-02-18 22:01:59 -0300

    adds 404 not found view

--- src/components/Content/OfferCard.vue
+++ src/components/Content/OfferCard.vue
@@ -20,6 +20,7 @@ export default {
 
     mounted() {
         this.getOffer();
+        console.log(this.$route.query.test);
     }
 
 }
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <p>not found</p>
+</template>
+
+<style scoped>
+
+p {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+}
+
+</style>
